beee8dac9e46b347ae8221467ebbbb56ce04429f
Sedanur Çevik <sdnrcvk@gmail.com>
2023-03-29 13:32:44 +0300

    Footer component designed

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,104 @@
 import React from 'react'
+import { MdFacebook  } from "react-icons/md";
+import { FaTwitter, FaLinkedin, FaGithub, FaGoogle, FaInstagram ,FaMedium } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer>
-    <p>Copyright © 2023 VUDES</p>
-  </footer>
+    <footer className="text-center text-lg-start text-light" style={{backgroundColor:"#0B2447"}}>
+
+      <section className="" style={{padding:"5px"}}>
+        <div className="container text-center text-md-start mt-5">
+          <div className="row mt-3">
+            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                <i className="fas fa-gem me-3"></i>VEDUS
+              </h6>
+              <p>
+                Bir çocuğun eğitimine gönüllü destek olarak başarıya giden o yolda beraber yürüyebilirsiniz.
+              </p>
+            </div>
+    
+            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                Sayfalar
+              </h6>
+              <p>
+                <a href="#!" className="text-reset">Ana Sayfa</a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">Özel Dersler</a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">Bize Ulaşın</a>
+              </p>
+            </div>
+    
+            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">
+                KATEGORİLER
+              </h6>
+              <p>
+                <a href="#!" className="text-reset">Türkçe</a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">Matematik</a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">Sosyal Bilimler</a>
+              </p>
+              <p>
+                <a href="#!" className="text-reset">Fen Bilimleri</a>
+              </p>
+            </div>
+    
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <h6 className="text-uppercase fw-bold mb-4">İLETİŞİM</h6>
+              <p><i className="fas fa-home me-3"></i> New York, NY 10012, US</p>
+              <p>
+                <i className="fas fa-envelope me-3"></i>
+                info@vedus.com
+              </p>
+              <p><i className="fas fa-phone me-3"></i> + 01 234 567 88</p>
+              <p><i className="fas fa-print me-3"></i> + 01 234 567 89</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Sosyal medya üzerinden bizlere ulaşabilirsiniz :</span>
+        </div>
+    
+        <div>
+          <a href="#!" className="me-4 text-light">
+            <MdFacebook/>
+          </a>
+          <a href="#!" className="me-4 text-light">
+            <FaTwitter/>
+          </a>
+          <a href="#!" className="me-4 text-light">
+            <FaGoogle/>
+          </a>
+          <a href="#!" className="me-4 text-light">
+            <FaInstagram/>
+          </a>
+          <a href="#!" className="me-4 text-light">
+            <FaLinkedin/>
+          </a>
+          <a href="#!" className="me-4 text-light">
+            <FaGithub/>
+          </a>
+          <a href="#!" className="me-4 text-light">
+            <FaMedium/>
+          </a>
+        </div>
+      </section>
+
+      <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
+        © 2023 &nbsp;
+        <a className="text-reset fw-bold" href="#!">vedus.com</a>
+      </div>
+    </footer>
   )
 }
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,49 +1,46 @@
 import React from 'react'
 import Link from 'next/link'
- 
+import { FaUser, FaSignOutAlt } from "react-icons/fa";
+
 export default function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+    <nav className="navbar navbar-expand-lg bg-warning">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
           <img src="logo.png" alt="VEDUS Logo" width="150"/>
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <div className='col-6'>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button class="btn btn-outline-success" type="submit">Search</button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <form className="d-flex flex-grow-1 justify-content-center" role="search">
+              <input className="form-control me-2 flex-grow-1" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn text-light" type="submit" style={{backgroundColor:"#0B2447"}}>Ara</button>
             </form>
-          </div>
-          <div className='col-6'>
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0" style={{float:"right"}}>
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" href="/">Ana Sayfa</Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" href="/courses">Özel Dersler</Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" href="/contact">Bize Ulaşın</Link>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><Link class="dropdown-item" href="/">Ana Sayfa</Link></li>
-                <li><Link class="dropdown-item" href="/courses">Özel Dersler</Link></li>
-                <li><Link class="dropdown-item" href="/contact">Bize Ulaşın</Link></li>
-
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
-            </li>
-          </ul>
-          </div>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" href="/">Ana Sayfa</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="/courses">Özel Dersler</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" href="/contact">Bize Ulaşın</Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <FaUser/> Sedanur Çevik
+                </a>
+                <ul className="dropdown-menu">
+                  <li><Link className="dropdown-item" href="/">Profilim</Link></li>
+                  <li><Link className="dropdown-item" href="/courses">Verdiğim Dersler</Link></li>
+                  <li><Link className="dropdown-item" href="/contact">Aldığım Dersler</Link></li>
+                  <li><hr className="dropdown-divider"/></li>
+                  <li><a className="dropdown-item" href="#">
+                    <FaSignOutAlt/> Çıkış Yap</a></li>
+                </ul>
+              </li>
+            </ul>
         </div>
       </div>
     </nav>
