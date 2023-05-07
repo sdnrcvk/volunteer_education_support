91878981e1223d0afa1a353a8b36d79b447b9d1e
Sedanur Çevik <sdnrcvk@gmail.com>
2023-05-07 20:40:34 +0300

    formik and yup validations added to contact page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,23 @@
 import React from 'react'
 import Layout from '@/components/Layout'
 import Head from 'next/head'
+import { useFormik } from 'formik'
+import validationSchema from '../validations/contactValidations'
 
 export default function contact() {
+
+  const { handleSubmit, handleChange, values, errors, touched, handleBlur, isSubmitting, setSubmitting, resetForm, dirty } = useFormik({
+    initialValues:{
+      fullname:'',
+      email:'',
+      message:'',
+    },
+    onSubmit:values=>{
+      console.log(values);
+    },
+    validationSchema,
+  })
+
   return (
     <>
       <Head>
@@ -38,59 +53,15 @@ export default function contact() {
                   <p className="text-gray-500 mb-2">info@vedus.com</p>
                 </div>
                 <div className="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-6/12 px-3 lg:px-6">
-                  <form>
+                  <form onSubmit={handleSubmit}>
                     <div className="form-group mb-6">
-                      <input type="text" className="form-control block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
-                        placeholder="Adınız"/>
+                      <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="fullname" name='fullname' placeholder="Adınız" value={values.fullname} onChange={handleChange} onBlur={handleBlur}/>
                     </div>
                     <div className="form-group mb-6">
-                      <input type="email" className="form-control block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
-                        placeholder="Email"/>
+                      <input type="email" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="email" name='email' placeholder="Email" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
                     </div>
                     <div className="form-group mb-6">
-                      <textarea className="
-                        form-control
-                        block
-                        w-full
-                        px-3
-                        py-1.5
-                        text-base
-                        font-normal
-                        text-gray-700
-                        bg-white bg-clip-padding
-                        border border-solid border-gray-300
-                        rounded
-                        transition
-                        ease-in-out
-                        m-0
-                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-                      " id="exampleFormControlTextarea13" rows="3" placeholder="Mesaj"></textarea>
+                      <textarea className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="message" name='message' rows="3" placeholder="Mesaj" value={values.message} onChange={handleChange} onBlur={handleBlur}></textarea>
                     </div>
                     <button type="submit" className="
                       w-full
---
+++ pages/validations/contactValidations.js
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+const contactValidations=Yup.object().shape({
+    fullname:Yup.string().required("İsim boş bırakılamaz"),
+    email:Yup.string().email("Geçerli bir e-posta adresi giriniz").required("E-posta boş bırakılamaz"),
+    message:Yup.string().required("Mesaj alanı boş bırakılamaz"),
+
+  })
+
+  export default contactValidations
